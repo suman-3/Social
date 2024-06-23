65385e0dec1fe37966a3f4a9ecc86d0de3200a8b
Suman Mondal <mondalsumanrock20@gmail.com>
2024-06-23 10:14:15 +0530

    stories example added

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -3,8 +3,99 @@ import React from "react";
 
 export const Stories = () => {
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md overflow-x-scroll text-sm">
-      <div className="flex gap-8">
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-x-scroll text-sm scrollbar-hide">
+      <div className="flex gap-8 w-max">
+        {/* Stories */}
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="story"
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium ">monX</span>
+        </div>
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="story"
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium ">monX</span>
+        </div>
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="story"
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium ">monX</span>
+        </div>
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="story"
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium ">monX</span>
+        </div>
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="story"
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium ">monX</span>
+        </div>
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="story"
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium ">monX</span>
+        </div>
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="story"
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium ">monX</span>
+        </div>
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="story"
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium ">monX</span>
+        </div>
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="story"
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium ">monX</span>
+        </div>
         <div className="flex flex-col gap-2 items-center cursor-pointer">
           <Image
             src="https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
